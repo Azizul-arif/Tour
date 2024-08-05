b8ff7508f146f8252d59d919a0ff253fc74607ab
Azizul Arif <azizularif94@gmail.com>
2024-08-06 03:14:41 +0600

    Tour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToursComponent } from './tours/tours.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToursComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
